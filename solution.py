95ecbc73ba103554bff8255ce37bfa686a849af7
Matiiii75 <matilinperiat.univ@gmail.com>
2025-01-23 16:15:31 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,21 +5,28 @@ class PCentreSolution:
         """
         self.entrepots = []  # Liste des entrepôts ouverts
         self.assignations = {}  # Dictionnaire client -> entrepôt
+        self.variables = {}
         self.distance_max = -1
- 
+    
     def ecrire_solution(self, path_solution: str):
         """
         Écrit la solution dans un fichier.
         """
+
         with open(path_solution, "w") as f:
             # Format d'écriture selon Appendix A
             text_sol = ''
             for entrepot_built in self.entrepots:
                 text_sol += f'{entrepot_built} '
             text_sol += '\n'
-            for entrepot in self.assignations.values():
-                text_sol += f'{entrepot} '
+            for j in range(len(self.assignations)):
+                text_sol += f'{self.assignations[j]} '
     
             text_sol += f'\n{self.distance_max}'
             f.write(text_sol)
+
+        
+        
+
+
  

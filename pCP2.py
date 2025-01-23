684e7b1109742208cc6a5445cb3fb9a10466a1e5
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2025-01-23 16:45:30 +0100

    Update pCP2.py

--- pCP2.py
+++ pCP2.py
@@ -15,6 +15,7 @@ class VersionRayon_1(ModelesPCentre):
         Implémente la version classique du problème.
         """
         if self.capacity:
+            print(self.data.Dk, '\n\n')
             # Modèle
             modele = pe.ConcreteModel(name = f'pCP2 avec capacité')
 
@@ -33,8 +34,7 @@ class VersionRayon_1(ModelesPCentre):
             modele.c2 = pe.ConstraintList()
             for i in range(self.data.nb_clients):
                 for j in range(self.data.nb_clients):
-                    modele.c2.add(modele.x[i, j] * self.data.d[i, j] <= quicksum([(self.data.Dk[k] - self.data.Dk[k-1])
-                                                    * modele.z[k] for k in range(1, len(self.data.Dk))]) + self.data.Dk[0])
+                    modele.c2.add(modele.x[i, j] <= quicksum([modele.z[k] for k in range(len(self.data.Dk)) if self.data.d[i,j] <= self.data.Dk[k]]))
                     
             modele.c3 = pe.ConstraintList()
             for j in range(self.data.nb_clients):
@@ -46,6 +46,11 @@ class VersionRayon_1(ModelesPCentre):
             modele.c4 = pe.ConstraintList()
             for i in range(self.data.nb_clients):
                 modele.c4.add(quicksum([q[j] * modele.x[i, j] for j in range(self.data.nb_clients)]) <= Q[i] * modele.y[i])
+            
+            modele.c5 = pe.ConstraintList()
+            for j in range(self.data.nb_clients):
+                for k, val_Dk in enumerate(self.data.Dk):
+                    modele.c5.add(expr = 1 - modele.z[k] <= quicksum([modele.y[i] if self.data.d[i,j] < val_Dk else 0 for i in range(self.data.nb_clients)]))
         else:
             # Modèle
             modele = pe.ConcreteModel(name = f'pCP2 sans capacité {self.name_model}')

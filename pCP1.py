3f51dcd6d37ed37b7acd3c99701b8513761c792c
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2025-01-23 11:01:03 +0100

    Update pCP1.py

--- pCP1.py
+++ pCP1.py
@@ -1,4 +1,5 @@
 from models import ModelesPCentre
+from solution import PCentreSolution
 import pyomo.environ as pe
 from pyomo.core import quicksum
 
@@ -38,11 +39,11 @@ class VersionClassique(ModelesPCentre):
         if self.capacity:
             #modele.Q = pe.Param(range(self.data.nb_points), name = 'Q', initialize = self.data.capacites, domain = pe.NonNegativeReals)
             #modele.q = pe.Param(range(self.data.nb_clients), name = 'q', initialize = self.data.demandes, domain = pe.NonNegativeReals)
-            Q = self.data.capacites[i]
-            q = self.data.demandes[j]
+            Q = self.data.Q
+            q = self.data.q
             
             modele.c4 = pe.ConstraintList()
-            for i in range(self.data.nb_points):
+            for i in range(self.data.nb_clients):
                 modele.c4.add(sum(q[j] * modele.x[i, j] for j in range(self.data.nb_clients)) <= Q[i] * modele.y[i])
         else:
             modele.c4 = pe.ConstraintList()
@@ -51,3 +52,21 @@ class VersionClassique(ModelesPCentre):
                     modele.c4.add(modele.x[i,j] <= modele.y[i])
 
         self.modele = modele
+
+    def extraire_solution(self, results):
+        solution = PCentreSolution()
+        if self.statut:
+            solution.distance_max = results.problem.lower_bound
+            for i in range(self.data.nb_clients):
+                entrepot_built = results.solution.variable[self.modele.y[i].getname()]['Value']
+                solution.entrepots.append(1 if entrepot_built else 0)
+                for j in range(self.data.nb_clients):
+                    assigned_to_i = results.solution.variable[self.modele.x[i, j].getname()]['Value']
+                    if assigned_to_i: solution.assignations[j] = i
+        else:
+            for i in range(self.data.nb_clients):
+                solution.entrepots.append(-1)
+                for j in range(self.data.nb_clients):
+                    solution.assignations[j] = -1
+        
+        return solution

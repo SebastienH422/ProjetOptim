a26ac650b0cee308188355a64815fa4b064d47a7
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2025-01-24 15:23:27 +0100

    Update projetOptimExpe.sh

--- projetOptimExpe.sh
+++ projetOptimExpe.sh
@@ -5,13 +5,13 @@ dossier_Solutions="..................................../Solutions"
 dossier_Experimentation="................................/Experimentations"
 
 
-Noeuds=(5 10 20 30) # Ensemble des valeur possible de n
+Noeuds=(300 500 1000 2000 3000) # Ensemble des valeur possible de n
 # Les valeurs de p pour chaque n
-P_5=(1 2 3) 
-P_10=(1 2 3)
-P_20=(2 5 6)
-P_30=(3 7 10)
-P_50=(5 12 16)
+P_300=(30 42 75 100) 
+P_500=(50 71 125 166)
+P_1000=(100 142 250 333)
+P_2000=(200 285 500 666)
+P_3000=(300 428 750 1000)
 
 index=(1 2 3 4 5) # Ensemble des valeurs possibles de i
 version=(1 2 3) # Les versions du problème

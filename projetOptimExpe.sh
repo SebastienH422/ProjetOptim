ef9ae8bd8aac105629924268b1bfd3d12eb7746c
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2025-02-01 14:19:40 +0100

    Update projetOptimExpe.sh

--- projetOptimExpe.sh
+++ projetOptimExpe.sh
@@ -36,3 +36,25 @@ for n in "${Noeuds[@]}"; do
     done    
     echo "Fin de résolution du  groupe de ${n} noeuds"
 done
+
+dossier_Solutions="Solutions_relax"
+dossier_Experimentation="Experimentations_relax"
+
+for n in "${Noeuds[@]}"; do
+    results_file="${dossier_Experimentation}/results_n${n}.txt"
+    rm -f "$results_file" 
+    echo "n p i v c erreur statut etat temps_creation temps gap obj obj_upper obj_lower" >> "${results_file}" # Entête du fichier de résultats
+    mesP="P_${n}[@]"
+    for p in "${!mesP}"; do
+        for i in "${index[@]}"; do
+            instance="${dossier_Instances}/n${n}p${p}i${i}"
+            for v in "${version[@]}"; do
+                for c in "${capacite[@]}"; do
+                    echo "Résolution relax: Instance_n${n}p${p}i${i} Modèle_v${v}_c${c}"
+                    python3 solver.py -v "${v}" -c "${c}" -d "${dossier_Instances}" -t "${TL}" -n "${n}" -p "${p}" -i "${i}" -s "${dossier_Solutions}" -r "${results_file}"
+                done
+            done
+        done 
+    done    
+    echo "Fin de résolution du  groupe de ${n} noeuds"
+done

98734b794e7ac5e39cd24712c1d7dfaaeeda5e4d
Sébastien HEIN <145024984+SebastienH422@users.noreply.github.com>
2025-02-03 13:06:50 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -25,6 +25,45 @@ class PCentreSolution:
             text_sol += f'\n{self.distance_max}'
             f.write(text_sol)
 
+        with open(path_solution + 'br', 'w') as f:
+            text_sol = '# Solutions brutes (valeurs des variables) \n'
+            for name, val in self.variables.items():
+                if name == 'x':
+                    text_sol += 'x: '
+                    for x_i in val:
+                        for x_ij in x_i:
+                            text_sol += f'{x_ij} '
+                        text_sol += '// '
+                    text_sol += '\n'
+                else:
+                    text_sol += f'{name}: '
+                    for yz in val:
+                        text_sol += f'{yz} '
+                    text_sol += '\n'
+            f.write(text_sol)
+        with open(path_solution + 'brm', 'w') as f:
+            text_sol = '# Solutions brutes (valeurs des variables) \n'
+            
+            for name, val in self.variables.items():
+                if name == 'x':
+                    max_x = 0
+                    for xi in val:
+                        max_x = max(max_x, max(xi))
+                    text_sol += 'x: '
+                    for x_i in val:
+                        for x_ij in x_i:
+                            text_sol += f'{round(x_ij / max_x)} '
+                        text_sol += '// '
+                    text_sol += '\n'
+                else:
+                    max_yzu = max(val) if max(val) else 1
+                    text_sol += f'{name}: '
+                    for yzu in val:
+                        text_sol += f'{round(yzu / max_yzu)} '
+                    text_sol += '\n'
+            f.write(text_sol)
+
+
         
         
 
